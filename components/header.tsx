404a889b126fff68adeed8a20c7b1ae28e09e17d
apykwan <apykwan@hotmail.com>
2024-10-29 01:19:27 -0700

    complete header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,58 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
+import LogoutButton from './logoutButton';
+
 function Header() {
-  return (
-    <header>
-      <div className="flex flex-col items-center space-y-5">
-        <div className="flex space-x-2 items-center">
-          <Image 
-            src="https://links.papareact.com/jne" 
-            alt="logo" 
-            height={10} 
-            width={50} 
-          />
-          <p className="text-blue-400">Welcome to Messenger</p>
+  const session = true;
+  function isLogin() {
+    if (session) {
+      return (
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-2">
+            <Image
+              className="rounded-full mx-2 object-contain" 
+              src="https://links.papareact.com/jne" 
+              alt="logo" 
+              height={10} 
+              width={50} 
+            />
+            <p>
+              <span className="text-blue-400">Welcome </span>
+              <span className="font-bold text-lg">John Doe</span>
+            </p>
+          </div>
+          <LogoutButton />
         </div>
-      </div>
-      <Link href="/auth/signin">Sign In</Link>
+      );
+    } else {
+      return (
+        <div className="flex flex-col items-center space-y-5">
+          <div className="flex space-x-2 items-center">
+            <Image 
+              src="https://links.papareact.com/jne" 
+              alt="logo" 
+              height={10} 
+              width={50} 
+            />
+            <p className="text-blue-400">Welcome to Messenger</p>
+          </div>
+          <Link 
+            href="/auth/signin" 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Sign In
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <header className="sticky top-0 z-50 bg-white/30 backdrop-blur-md shadow-sm p-5">      
+      {isLogin()}
     </header>
   );
 }
---
+++ components/messsageList.tsx
@@ -0,0 +1,7 @@
+function MessageList() {
+  return (
+    <div>Messages go here...</div>
+  );
+}
+
+export default MessageList;
